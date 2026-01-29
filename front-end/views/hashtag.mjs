d409302e1def04afaa752eecb377c6672857a72e
Rihanna P <114884466+RihannaP@users.noreply.github.com>
2026-01-29 17:07:32 +0000

    fix loop in hashtag by checking cleanHashtag

--- front-end/views/hashtag.mjs
+++ front-end/views/hashtag.mjs
@@ -17,8 +17,10 @@ import {createHeading} from "../components/heading.mjs";
 function hashtagView(hashtag) {
   destroy();
 
+  const cleanHashtag = hashtag.startsWith('#') ? hashtag : `#${hashtag}`;
+  if (state.currentHashtag !== cleanHashtag) {
   apiService.getBloomsByHashtag(hashtag);
-
+  }
   renderOne(
     state.isLoggedIn,
     getLogoutContainer(),
